882906d82896031e913f905086f89324851ef3f2
Raphael Tournafond <raphael.tournafond@gmail.com>
2023-03-26 22:56:21 +0200

    feat: add FastLed connection setup and datapin

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 6
+#define DATA_PIN 13
+
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
