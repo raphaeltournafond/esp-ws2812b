5bceef954569170974434da1941131a2b8e70595
Raphael Tournafond <raphael.tournafond@gmail.com>
2023-03-27 00:25:59 +0200

    feat: add K2000 effect

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,17 @@ void setup() {
 }
 
 void loop() {
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  FastLED.show();
+  // K2000
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
+  }
+  for (int i = NUM_LEDS-2; i > 0; i--) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
+  }
 }
