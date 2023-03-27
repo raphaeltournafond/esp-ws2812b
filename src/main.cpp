fda175b57408ed54321f39d0466689b92f8d0fab
Raphael Tournafond <raphael.tournafond@gmail.com>
2023-03-27 14:25:09 +0200

    feat: add brightness controls

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,22 @@
 
 #define NUM_LEDS 6
 #define DATA_PIN 13
+#define BRIGTHNESS 255
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGTHNESS);
 }
 
 void loop() {
-  // K2000
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
+    // Rainbow cycle
+  for (int j = 0; j < 256; j++) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV((i * 256 / NUM_LEDS) + j, 255, 255);
+    }
     FastLED.show();
-    delay(100);
-    leds[i] = CRGB::Black;
-  }
-  for (int i = NUM_LEDS-2; i > 0; i--) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(100);
-    leds[i] = CRGB::Black;
+    delay(20);
   }
 }
