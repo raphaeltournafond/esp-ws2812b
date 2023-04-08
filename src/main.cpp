4348471652f227bdb49d94f408418570cb27ebea
Raphael Tournafond <raphael.tournafond@gmail.com>
2023-04-09 00:15:54 +0200

    feat!(effect): add effect selection

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,24 @@ const char* password = "InstaLED";
 // create server instance
 AsyncWebServer server(80);
 
-// Variable to store the HTTP request
-String header;
-
 #define NUM_LEDS 6
 #define DATA_PIN 13
 #define BRIGTHNESS 255
 
 CRGB leds[NUM_LEDS];
+
+// Array of effect names
+const String effectNames[] = {
+  "Rainbow",
+  "K2000",
+  "Wipe",
+  "Fade",
+  "Twinkle",
+  "Comet"
+};
+
+int currentEffectIndex = -1;
+CRGB currentColor = CRGB::Red;
 
 void setup() {
   // Webserver setup
@@ -36,11 +46,32 @@ void setup() {
     String html = "<html><body>";
     html += "<h1>Swee.brz le gros bg</h1>";
     html += "<input type='color' id='colorPicker' value='#FF0000'>";
-    html += "<button onclick='sendColor()'>Send Color</button>";
+    html += "<button onclick='sendColor()'>Choisir une couleur fixe</button>";
+    html += "<br />";
+    html += "<label for='effectSelect'>Choisir un effet:</label>";
+    html += "<select id='effectSelect' onchange='sendEffect()'>"; // Combo box for selecting effects
+    html += "<option value='0'>Rainbow</option>";
+    html += "<option value='1'>K2000</option>";
+    html += "<option value='2'>Wipe</option>";
+    html += "<option value='3'>Fade</option>";
+    html += "<option value='4'>Twinkle</option>";
+    html += "<option value='5'>Comet</option>";
+    html += "</select>";
     html += "<script>";
     html += "function sendColor() {\n";
     html += "  var color = document.getElementById('colorPicker').value;\n";
     html += "  fetch('/setColor?color=' + encodeURIComponent(color))\n";
+    html += "    .then(response => {\n";
+    html += "      if (response.ok) {\n";
+    html += "        return response.text();\n";
+    html += "      } else {\n";
+    html += "        throw new Error('Error: ' + response.status);\n";
+    html += "      }\n";
+    html += "    })\n";
+    html += "}\n";
+    html += "function sendEffect() {\n";
+    html += "  var effectSelect = document.getElementById('effectSelect');\n";
+    html += "  fetch('/setEffect?effect=' + encodeURIComponent(effectSelect.selectedIndex))\n";
     html += "    .then(response => {\n";
     html += "      if (response.ok) {\n";
     html += "        return response.text();\n";
@@ -70,8 +101,23 @@ void setup() {
 
     FastLED.show();
 
+    currentColor = color;
+    currentEffectIndex = -1;
+
     // Send a response to the client
     request->send(200, "text/plain", "Color received: " + color);
+  });
+
+  // Set up a handler for the HTTP GET request with URL parameters
+  server.on("/setEffect", HTTP_GET, [](AsyncWebServerRequest *request){
+    int effectIndex = request->getParam("effect")->value().toInt();
+    Serial.print("Received effect: ");
+    Serial.println(effectNames[effectIndex]);
+
+    currentEffectIndex = effectIndex;
+
+    // Send a response to the client
+    request->send(200, "text/plain", "Effect received: " + effectNames[currentEffectIndex]);
   });
 
   // Start the server
@@ -82,13 +128,126 @@ void setup() {
   FastLED.setBrightness(BRIGTHNESS);
 }
 
+void rainbowEffect(int speed) {
+  static uint8_t hue = 0; // Static variable to store current hue value
+
+  // Increment the hue value and update LED colors
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue + i * 256 / NUM_LEDS, 255, 255);
+  }
+  FastLED.show();
+
+  // Increment hue for next iteration based on speed parameter
+  hue += speed;
+  delay(50);
+}
+
+// Define the variables for the K2000 effect
+int k2000Pos = 0;
+int k2000Dir = 1;
+
+// Function to implement the K2000 effect
+void K2000Effect(CRGB color) {
+  // Update the K2000 effect
+  k2000Pos += k2000Dir;
+  
+  // Reverse direction if we hit the end of the strip
+  if (k2000Pos == 0 || k2000Pos == NUM_LEDS - 1) {
+    k2000Dir *= -1;
+  }
+  
+  // Set all LEDs to black
+  FastLED.clear();
+  
+  // Set the color of the moving LED
+  leds[k2000Pos] = color;
+  
+  // Show the updated LED colors
+  FastLED.show();
+  
+  // Delay to control the speed of the effect
+  delay(100);
+}
+
+// Function to implement the "Color Wipe" effect
+void ColorWipe(CRGB color) {
+  // Loop through each LED in the strip
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Set the current LED to the desired color
+    leds[i] = color;
+    FastLED.show();  // Update the LED strip
+    delay(100);  // Delay to control the speed of the effect
+  }
+  
+  // Turn off all LEDs
+  FastLED.clear();
+  FastLED.show();
+}
+
+void fadeEffect(CRGB color1, CRGB color2, int duration) {
+  // Fade between two colors with a specified duration
+  for (int i = 0; i <= 255; i++) {
+    CRGB color = blend(color1, color2, i);
+    fill_solid(leds, NUM_LEDS, color);
+    FastLED.show();
+    delay(duration / 255);
+  }
+}
+
+void twinkleEffect(CRGB color) {
+  // Randomly turn on and off LEDs with a specified color
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (random8() < 64) {
+      leds[i] = color;
+    } else {
+      leds[i] = CRGB::Black;
+    }
+  }
+  FastLED.show();
+}
+
+void cometEffect(CRGB color, int tailSize, int speed) {
+  // Move a comet with a tail across the LED strip
+  for (int i = 0; i < NUM_LEDS + tailSize; i++) {
+    for (int j = 0; j < NUM_LEDS; j++) {
+      if (j == i) {
+        leds[j] = color;
+      } else if (j > i && j <= i + tailSize) {
+        leds[j] = color.fadeToBlackBy(128);
+      } else {
+        leds[j] = CRGB::Black;
+      }
+    }
+    FastLED.show();
+    delay(speed);
+  }
+}
+
 void loop() {
-  // Rainbow cycle
-  // for (int j = 0; j < 256; j++) {
-  //   for (int i = 0; i < NUM_LEDS; i++) {
-  //     leds[i] = CHSV((i * 256 / NUM_LEDS) + j, 255, 255);
-  //   }
-  //   FastLED.show();
-  //   delay(5);
-  // }
+  switch (currentEffectIndex)
+  {
+  case 0:
+    rainbowEffect(12);
+    break;
+  case 1:
+    K2000Effect(currentColor);
+    break;
+  case 2:
+    ColorWipe(currentColor);
+    break;
+  case 3:
+    fadeEffect(currentColor, CRGB::Black, 100);
+    break;
+  case 4:
+    twinkleEffect(currentColor);
+    delay(100);
+    break;
+  case 5:
+    cometEffect(currentColor, 2, 100);
+    break;
+  default:
+    fill_solid(leds, NUM_LEDS, currentColor);
+    FastLED.show();
+    break;
+  }
 }
